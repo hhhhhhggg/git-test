a94cc30a2c871ea8171692c4744a277b95cb6158
hhhhgg <1366815119@qq.com>
2023-02-03 19:03:15 +0800

    hot-fix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,9 +1,11 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("", "hello git!");
-        System.out.printf("", "hello git2!");
-        System.out.printf("", "hello git3!");
-        System.out.printf("", "hello git4!");
+        System.out.printf( "hello git!");
+        System.out.printf("hello git2!");
+        System.out.printf( "hello git3!");
+        System.out.printf( "hello git4!");
+        System.out.printf( "hot-fix text!");
+
 
 
     }
