cea612b26a34606fbd1d78980fec3d586c88f7e6
hhhhgg <1366815119@qq.com>
2023-02-03 18:43:19 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.printf("", "hello git!");
         System.out.printf("", "hello git2!");
+        System.out.printf("", "hello git3!");
 
     }
 }
